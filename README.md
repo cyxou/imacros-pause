419f8e97bccefb9b692642e130dae9aa71ce01f4
cyxou <acyxou@gmail.com>
2016-04-07 08:56:59 +0300

    Rewrite pause function utilizing nsITimer

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ A JavaScript alternative to iMacros WAIT SECONDS function.
 
 Designed to work with Browserify.
 
+All credits go to [Alexander Akhmetkhanov](https://github.com/Sashka66Rus)
+
 ### Installation
 ```sh
 npm install imacros-pause --save
--- index.js
+++ index.js
@@ -1,23 +1,50 @@
 'use strict';
 
-var myWindow = Components.classes['@mozilla.org/appshell/appShellService;1']
-  .getService(Components.interfaces.nsIAppShellService)
-  .hiddenDOMWindow;
+/* global global */
 
-function pause(seconds) {
-  iimDisplay('Waiting ' + seconds + 'sec.');
+// global is iMacros Sandbox object that is supposed to be set in banner like this:
+// var global = this;
 
-  var PA = myWindow.setInterval(function() {
-    iimDisplay('Waiting ' + (--seconds) + 'sec.');
-  }, 1000);
+var Ci = Components['interfaces'];
+var Cc = Components['classes'];
 
-  myWindow.setTimeout(
-    function() {
-      myWindow.clearInterval(PA);
-      iimDisplay('');
-      iimPlayCode('');
-    }, (seconds * 1000));
-    iimPlayCode('PAUSE');
-}
+global['timer'] = Cc['@mozilla.org/timer;1']
+  ['createInstance'](Ci['nsITimer']);
 
-module.exports = pause;
+var chrome = window['QueryInterface'](Ci['nsIInterfaceRequestor'])
+  ['getInterface'](Ci['nsIWebNavigation'])
+  ['QueryInterface'](Ci['nsIDocShellTreeItem'])['rootTreeItem']
+  ['QueryInterface'](Ci['nsIInterfaceRequestor'])
+  ['getInterface'](Ci['nsIDOMWindow']);
+
+// Add onclick handler to the stop button on iMacros' main panel
+chrome['iMacros']['panel']['sidebar']['document']
+  ['getElementById']('im-stopplay-button')['onclick'] = function() {
+    global['timer']['cancel']();
+};
+
+// Add onclick handler to the stop button on iMacros' minified panel
+chrome['document']['getElementById']('imacros-info-panel-stop-button')['onclick'] = function() {
+  global['timer']['cancel']();
+};
+
+module.exports = function pause(seconds) {
+  seconds--;
+  var display = seconds;
+  iimDisplay('Waiting ' + display + ' sec.');
+
+  var delay = {
+    observe: function() {
+      seconds--;
+      iimDisplay('Waiting ' + (--display) + ' sec.');
+      if (seconds <= 0) {
+        global['timer']['cancel']();
+        iimDisplay('');
+        iimPlayCode('');
+      }
+    }
+  };
+
+  global['timer']['init'](delay, 1000, Ci['nsITimer']['TYPE_REPEATING_PRECISE_CAN_SKIP']);
+  iimPlayCode('pause');
+};
